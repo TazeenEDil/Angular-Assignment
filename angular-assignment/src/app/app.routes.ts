0dfde7dc4bef535ed7f25a8a1cecf2c2d8106a9e
TazeenEDil <tazeenedil470@gmail.com>
2026-01-02 01:57:56 +0500

    added login+modal+inspectors+guards

--- angular-assignment/src/app/app.routes.ts
+++ angular-assignment/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
-import { EmployeeList } from './components/employee-list/employee-list';
+import { Login } from './components/login/login';
+import { EmployeeList} from './components/employee-list/employee-list';
 import { EmployeeForm } from './components/employee-form/employee-form';
-import { EmployeeUpdate } from './components/employee-update/employee-update';
 import { EmployeeDetails } from './components/employee-details/employee-details';
+import { EmployeeUpdate } from './components/employee-update/employee-update';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-  { path: '', component: EmployeeList },
-  { path: 'employee/add', component: EmployeeForm },
-  { path: 'employee/edit/:id', component: EmployeeUpdate },
-  { path: 'employee/:id', component: EmployeeDetails },
-  { path: '**', redirectTo: '' }
+  { path: 'login', component: Login},
+  { path: '', component: EmployeeList, canActivate: [authGuard] },
+  { path: 'employee/add', component: EmployeeForm, canActivate: [authGuard] },
+  { path: 'employee/:id', component: EmployeeDetails, canActivate: [authGuard] },
+  { path: 'employee/edit/:id', component: EmployeeUpdate, canActivate: [authGuard] },
+  { path: '**', redirectTo: 'login' }
 ];
